047b1c82939aa4a022c0555e35ab4847f5b0f05a
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2020-10-16 16:37:07 -0500

    changed footer info

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,11 +4,11 @@ const Footer = () => (
   <footer className="container mx-auto py-16 px-3 mt-48 mb-8 text-gray-800">
     <div className="flex -mx-3">
       <div className="flex-1 px-3">
-        <h2 className="text-lg font-semibold">About Us</h2>
-        <p className="mt-5">(940) 255-2038</p>
+        <h2 className="text-lg font-semibold">North Texas Karate Academy</h2>
         <a href="https://goo.gl/maps/ADx3LN7mHpesF6or7" className="mt-5">
           3107 State Highway 101 Bridgeport, TX 76426
         </a>
+        <p className="">(940) 255-2038</p>
       </div>
       <div className="flex-1 px-3">
         <h2 className="text-lg font-semibold">Links</h2>
