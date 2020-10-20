1e797d3ea2788c423183320f69c5952fd39c3ddf
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2020-10-20 17:55:42 -0500

    added text to tract

--- src/components/TractFlipper.js
+++ src/components/TractFlipper.js
@@ -23,16 +23,16 @@ export default function TractFlipper({ width }) {
   return (
     <div>
       <ReactCardFlipper
-        behavior="click"
         levitate={true}
         width={width}
-        height={500}
+        height={550}
         className="container mx-auto cursor-pointer"
       >
         <div
           className="px-10 h-full flex flex-col justify-center items-center w-full border border-black"
           style={{ backgroundImage: `url(${rainbow})`, backgroundSize: 'cover' }}
         >
+          <p className="font-bold text-xl">click</p>
           <h3 className="font-bold text-5xl lg:text-6xl text-center text-black ">
             Smile God Loves You
           </h3>
@@ -53,12 +53,58 @@ export default function TractFlipper({ width }) {
               );
             })}
           </ol> */}
+          {/* <span className="text-brightYellow"></span> */}
+          {/* <span className="font-bold"></span> */}
         </div>
-        <div className="bg-black text-white h-full px-10 -full overflow-auto flex flex-col justify-center items-center ">
-          <p className=" text-xl  ">
-            For God so loved the world, that he gave his only begotten Son, that whosoever believeth
-            in him should not perish, but have everlasting life. For God sent not his Son into the
-            world to condemn the world; but that the world through him might be saved.
+        <div className=" h-full px-2 overflow-x-hidden flex flex-col justify-center items-center border border-solid border-black">
+          <p className="">
+            <span className="text-primeRed">John 3:16-17</span> says "For God so loved the world,
+            that he gave his only begotten Son, that whosoever believeth in him should not perish,
+            but have everlasting life. For God sent not his Son into the world to condemn the world;
+            but that the world through him <span className="font-bold">might be saved</span>." Most
+            people know about
+            <span className="text-primary"> John 3:16-17</span>, but ignore
+            <span className="text-yellow-600"> verses 18-19</span>, wich say, "He that believeth on
+            him is not condemned: but he that believeth not is{' '}
+            <span className="font-bold">condemned already </span>condemned already, because he hath
+            not believed in the name of the only begotten Son of God. And this is the condemnation,
+            that light is come into the world, and{' '}
+            <span className="font-bold">men loved darkness </span>rather than light,{' '}
+            <span className="font-bold">because their deeds were evil</span>." Friend, don't be
+            deceived! Just because God "loves you", <span className="font-bold">doesn't mean</span>{' '}
+            that He loves the way your life (<span className="font-bold">if you are sinning</span>)
+            or the He won't <span className="text-purple-600">condemn you to hell</span> for you
+            sin. <span className="text-primeRed">Romans 5:8</span> says, "But God demonstrates His
+            own love toward us, in that while we were still sinners, Christ died for us." God has an{' '}
+            <span className="text-green-600">unconditional love </span>for you, in this way: He sent
+            jesusto die for <span className="font-bold">YOU and He wants YOU</span> to have
+            everlasting life. God has a <span className="font-bold tex-3xl">Holy Hatred</span>, for
+            sinners. <span className="text-purple-600">Psalm 5:4-5</span> says, "For you are nat a
+            God who takes pleasure in wickedness,{' '}
+            <span className="font-bold">nor shall evil dwell with you</span>. The boastful shall not
+            stand in Your sight; you <span className="font-bold">hate all workers of iniquity</span>
+            ." <span className="text-primeYellow">Psalm 11:5-6</span> says, "The Lord tests the
+            righteous, but the wicked and the one who loves violence{' '}
+            <span className="font-bold">His soul hates</span>. Upon the wicked He will rain coals;{' '}
+            <span className="font-bold">fire and brimstone</span> and a burning wind shall be the
+            portion of their cup." The question is not whether or not God loves you. The question
+            whether or not God loves the way you are living you life, whether or not{' '}
+            <span className="text-purple-600 text-bold">YOU LOVE GOD</span>. Jesus said, "
+            <span className="font-bold">If you love Me</span>, keep My commandments"(
+            <span className="text-green-600"></span>). If you are lying, stealing, lusting, getting
+            drunk, fornicating, doing drugs or <span className="font-bold">sinning at all</span>,
+            you currently <span className="text-primeRed text-bold">DON'T LOVE GOD</span>. You need
+            to, "Repent therefore and be converted, that your sins may be blotted out, so that times
+            of refreshing may come from the presence of the Lord"(
+            <span className="text-purple-600">Acts 3:19</span>). There{' '}
+            <span className="text-yellow-600">IS HOPE</span> for you, but{' '}
+            <span className="font-bold">ONLY </span>in Jesus. He commands you to{' '}
+            <span className="font-bold">Forsake</span> all of your sins,{' '}
+            <span className="font-bold">Trust</span> in Him and{' '}
+            <span className="font-bold">Follow</span> Him.{' '}
+            <span className="text-green-600">
+              <span className="font-bold">DO IT TODAY,</span> before it's too late!
+            </span>
           </p>
         </div>
       </ReactCardFlipper>
