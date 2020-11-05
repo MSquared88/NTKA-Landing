27f628a36bdc67d6334883c76c3adba5dda8274d
Matthew Meeves <matthew.meeves@gmail.com>
2020-11-05 17:52:02 -0600

    made contact divs larger on smaller views

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,7 +8,7 @@ export default function Contact() {
       className="shadow-md rounde pt-6 pb-8 mb-4 flex flex-col items-center"
     >
 
-      <div className="mb-4 flex flex-col items-center w-1/2">
+      <div className="mb-4 flex flex-col items-center w-3/4 lg:w-1/2">
         <label className="text-white text-left text-xl font-bold mb-2 w-full">Name</label>
         <input
           type="text"
@@ -17,7 +17,7 @@ export default function Contact() {
           className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
         />
       </div>
-      <div className="mb-6 flex flex-col items-center w-1/2">
+      <div className="mb-6 flex flex-col items-center w-3/4 lg:w-1/2">
         <label className=" text-white text-left text-xl font-bold mb-2 w-full">Email</label>
         <input
           type="email"
@@ -26,7 +26,7 @@ export default function Contact() {
           className="shadow appearance-none border rounded w-full py-2  text-black leading-tight focus:outline-none focus:shadow-outline"
         />
       </div>
-      <div className="mb-6 flex flex-col items-center w-1/2">
+      <div className="mb-6 flex flex-col items-center w-3/4 lg:w-1/2">
         <label className="text-white text-left text-xl font-bold mb-2 w-full">Subject</label>
         <input
           type="text"
@@ -35,7 +35,7 @@ export default function Contact() {
           className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
         />
       </div>
-      <div className="mb-6 flex flex-col items-center w-1/2">
+      <div className="mb-6 flex flex-col items-center w-3/4 lg:w-1/2">
         <label className="text-white text-left text-xl font-bold mb-3 w-full">Message</label>
         <textarea
           name="message"
