aa40853f35efa85fa5ec0386ed54d48297de4b64
Matthew Meeves <matthew.meeves@gmail.com>
2021-02-11 16:24:57 -0600

    added google tag to plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,29 @@ module.exports = {
       options: {
         tailwindConfig: `tailwind.config.js`
       }
+    },
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: 'G-XGG52CH4ZQ',
+
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: 'gatsby' },
+
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        routeChangeEventName: 'YOUR_ROUTE_CHANGE_EVENT_NAME'
+      }
     }
   ]
 };
