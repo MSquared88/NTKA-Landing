1e3c5c5428e115691afc5c5870fe5fad5a2b5cb9
Matthew Meeves <matthew.meeves@gmail.com>
2021-08-02 16:04:54 -0500

    switched to yarn and removed tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,10 @@
 module.exports = {
   plugins: [
     {
-      resolve: 'gatsby-plugin-google-tagmanager',
-      options: {
-        id: 'GTM-PGHXSSQ',
-        includeInDevelopment: false
-      }
-    },
-    {
       resolve: `gatsby-theme-codebushi`,
       options: {
-        tailwindConfig: `tailwind.config.js`
-      }
-    }
-  ]
+        tailwindConfig: `tailwind.config.js`,
+      },
+    },
+  ],
 };
